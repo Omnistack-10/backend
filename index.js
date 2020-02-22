74ed81f9c50c59327b30532367ce2534dc4d835d
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2020-02-22 11:40:03 -0300

    ajustes finais para aplicação restfull

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require('express'); // criação e controle de rotas
 
 const app = express(); // iniciando objeto para iniciar a aplicação
 
+// app.use() é uma regra que vai ser válido por todas as regras da aplicação
+// aplicação agora vai entender requições com corpo no formato json
+app.use(express.json())
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmtros
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+//               muito usado: GET. 
+//               Ex: /users?pesquisar=Batatinha
+// Route Params: request.params (Identificar uma recurso na alteração ou remoção)
+//               muito usado: PUT, DELETE
+//               Ex: /user/1
+// Body: request.body (Dados para criação ou alteração de um registro)
+//       muito usado: POST
+
 // rota principal
 // a array function recebe dois parâmetros fixos
 // request é a requisição e response é a reposta da resquisição
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello Omnistack'});
-}); 
+app.post('/users/:id', (request, response) => {
+    console.log(request.body);
+    return response.json({ message: 'Hello Hello' });
+});
 
 app.listen(3333); // configurar uma porta para rodar a aplicação
 
