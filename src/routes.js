747ffebed6dbb6bbc953d4ef386ada1be25a8e45
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2020-02-22 15:49:28 -0300

    cadastro de devs concluido

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,37 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-// rota principal
 // a array function recebe dois parâmetros fixos
 // request é a requisição e response é a reposta da resquisição
-routes.post('/users/:id', (request, response) => {
-    console.log(request.body);
-    return response.json({ message: 'Hello Hello' });
+routes.post('/devs', async (request, response) => {
+    const { github_username, techs, latitude, longitude } = request.body;
+
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const { name = login, avatar_url, bio } = apiResponse.data;
+
+    // metodo trim remove espaços antes e depois de uma string
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+    }
+
+    // função para salvar Dev
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+    });
+
+    return response.json(dev);
 });
 
 // exportas as rotas para a aplicação passa a reconhecer as rotas
