bf258f36d8390637726864f5b148d4c6e202d04b
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2020-02-23 22:27:09 -0300

    extraindo o servidor http da aplicação express

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express'); // criação e controle de rotas
 const mongoose = require('mongoose'); // criação do objeto para banco de dados
 const cors = require('cors');
+const http = require('http'); // fazer que a aplicação responda a requições http e as requições websockets
 const routes = require('./routes'); // importantando as rotas
 
 const app = express(); // iniciando objeto para iniciar a aplicação
+const server = http.Server(app); // extrai da aplicação express, o servidor http, que está fora da aplicação express
 
 // https://account.mongodb.com/account/login
 mongoose.connect('mongodb+srv://maycon:semsenha@cluster0-nbq7g.mongodb.net/test?retryWrites=true&w=majority', {
@@ -18,7 +20,7 @@ app.use(express.json())
 app.use(routes);
 
 
-app.listen(3333); // configurar uma porta para rodar a aplicação
+server.listen(3333); // configurar uma porta para rodar a aplicação
 
 // Métodos HTTP: GET, POST, PUT, DELETE
 
